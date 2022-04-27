b4fe91075de58dbae33feed0124eb853c9b1db2c
bkfg12335 <erwan.quinot@gmail.com>
2022-04-27 16:52:25 +0200

    finish

--- Sound.js
+++ Sound.js
@@ -9,8 +9,15 @@ var gains = [];
 var audioElem;
 var test = 0;
 
+//focntion permettant de lancer l'audio
+
 function AudioBegin() {
+
+	//lancemeent de l'audio
+
 	audioContext.resume();
+
+	//set up et mise en place de tous les noeuds : Audio --> Gain --> PositionAudio --> ContextAudio
 
 	for (var i = 1; i <= nbSound; i ++) {
 		sourcePlaces.push(audioContext.createPanner());
@@ -25,45 +32,79 @@ function AudioBegin() {
 		gains[i-1].gain.setValueAtTime(0.3, 0);
 	}
 
+	//placement du listener
+
 	listener.setPosition(0, 0, 0);
 }
 
+//fonction allant récupérer la bande son sélectionnée
+
 function AudioChoose(inc, path) {
 	audioElem = document.getElementById("PlayPause" + inc);
+
+	//mets en pause le son concerné à chaque changement
+
 	PlayPause("PlayPause" + inc, inc, "Pause");
+
+	//vérifie qu'un chemin a bien été entré
+
 	if (path[0] && path[0] != "No Sound") {
+
+		//affecte le chemin du fichier audio selon le mode en cours d'utilisation
+
 		if (path[0].name) {
 			document.getElementById("chooseSound" + inc).src = "Audio/" + path[0].name;
 		}
 		else {
 			document.getElementById("chooseSound" + inc).src = "Audio/" + path[0];
 		}
+
+		//affiche le bouton play/pause et la source concernés
+
 		audioElem.style.visibility = "visible";
 		audioElem.style.position = "relative";
 		document.getElementById("src" + inc).style.visibility = "visible";
 	}
 	else {
+
+		//réinitialise le chemin du fichier audio lorsque l'uilisateur n'a rien choisi
+
 		document.getElementById("chooseSound" + inc).src = "";
+
+		//cache le bouton play/pause et la source concernés
+
 		audioElem.style.visibility = "hidden";
 		audioElem.style.position = "absolute";
 		document.getElementById("src" + inc).style.visibility = "hidden";
 	}
 }
 
+//fonction permettant d'update la position des sources sonores
+
 function AudioUpdatePos(inc, pos){
 	sourcePlaces[inc-1].setPosition(pos[inc-1][0], pos[inc-1][1], pos[inc-1][2]);
 }
+
+//fonction permettant d'update l'orientation de l'utilisateur
 
 function AudioUpdateOr(or){
 	listener.setOrientation(Math.cos((or-90)*Math.PI/180), 0, Math.sin((or-90)*Math.PI/180), 0, 1, 0);
 }
 
+//fonction permettant de jouer et mettre en pause les différentes bandes sons
+
 function PlayPause(id, inc, playing) {
 	if (playing == "Pause"){
+
+		//si le bouton avait la valeur pause, il met l'audio concerné en pause et change la valeur du bouton en play
+
 		document.getElementById("chooseSound" + inc).pause()
 		document.getElementById(id).value = "Play";
 	}
 	else {
+
+		//si le bouton avait la valeur play, il met l'audio concerné en play et change la valeur du bouton en pause
+
 		document.getElementById("chooseSound" + inc).play();
 		document.getElementById(id).value = "Pause";
 	}
--- TestPosOr.js
+++ TestPosOr.js
@@ -10,16 +10,25 @@ var gameMode = "with";
 var	file;
 var	provided;
 
-function Display() {
-	SetId();
 
+//Fonction s'activant une unique fois après l'appui sur le bouton begin. Elle affiche l'ensemble du site
+
+function Display() {
+
+	//Instanciation des ids pour les blocs créés à partir de choose.html
+
+	SetId();
 
 	file = document.getElementsByClassName("FileChoose");
 	provided = document.getElementsByClassName("AudioChoose");
 
+	//Cache le bouton begin et affiche tout le reste
+
 	document.getElementsByClassName("Using")[0].style.visibility = "visible";
 	document.getElementsByClassName("BeginButton")[0].style.visibility = "hidden";
 	document.getElementsByClassName("BeginButton")[0].style.position = "absolute";
+
+	//Mets les boutons play/pauses en invisible et cachent les blocs liés au mode provide
 
 	for (var i = 1; i <= nbSound; i++) {
 		document.getElementById("PlayPause" + i).style.visibility = "hidden";
@@ -29,20 +38,32 @@ function Display() {
 		provided[i-1].style.position = "absolute";
 	}
 
+	//Active l'instanciation de l'audio
+
 	AudioBegin();
 }
+
+//fonction s'activant lorsque le nombre d'audios est changé. Elle affiche le nombre de blocs indiqué et cache le reste
 
 function NbAudios(Nb) {
 	nbAudio = Nb
 	for (var i = 1; i < nbSound; i++) {
 		if (i < Nb) {
+
+			//affiche les blocs tant qu'on n'en a pas suffisamment
+
 			document.getElementsByClassName("Flex")[i].style.visibility = "visible";
 			document.getElementsByClassName("Flex")[i].style.position = "relative";
 		}
 		else {
+
+			//cache les blocs en trop
+
 			document.getElementsByClassName("Flex")[i].style.visibility = "hidden";
 			document.getElementsByClassName("Flex")[i].style.position = "absolute";
 			temp = i+1;
+
+			//Réinitialise les valeurs des blocs du mode provide (on peut pas le faire pour les "file choose")
 
 			document.getElementById("audioChoose"+temp).value = "No Sound";
 			AudioChoose(temp, ["No Sound"]);
@@ -50,46 +71,84 @@ function NbAudios(Nb) {
 	}
 }
 
+//fonction s'activant lorsqu'on change de mode avec les radios button. Elle alterne l'affichage entre les modes sélection et provide
+
 function ModeChange(mode) {
 	gameMode = mode;
 	if (mode=="with") {
 		for (var i = 0; i < nbSound; i++) {
+
+			//cache les blocs provide et affiche les blocs sélections
+
 			file[i].style.visibility = "";
 			file[i].style.position = "relative";
 			provided[i].style.visibility = "hidden";
 			provided[i].style.position = "absolute";
+
+			//Mets en place la disposition en fonction des audios déjà choisis
+
 			temp = i+1;
 			AudioChoose(temp, document.getElementById("file"+temp).files);
+
+			//Appelle de la fonction NbAudios pour effacer les blocs superflus
+
 			NbAudios(nbAudio);
 		}
 	}
 	else {
 		for (var i = 0; i < nbSound; i++) {
+
+			//cache les blocs sélections et affiche les blocs provide
+
+
 			file[i].style.visibility = "hidden";
 			file[i].style.position = "absolute";
 			provided[i].style.visibility = "";
 			provided[i].style.position = "relative";
+
+			//Mets en place la disposition en fonction des audios déjà choisis
+
 			temp = i+1;
 			AudioChoose(temp, [document.getElementById("audioChoose"+temp).value]);
+
+			//Appelle de la fonction NbAudios pour effacer les blocs superflus
+
 			NbAudios(nbAudio);
 		}
 	}
 }
 
+//fonction permettant d'update la position
+
 function UpdatePos(inc, dir, value) {
+
+	//On affiche le cercle représentant la source sonore à sa nouvelle position
+
 	positions[inc-1][dir] = value;
 	tempX = positions[inc-1][0]*20 - 6;
 	tempY = -positions[inc-1][2]*20 + 98;
 	document.getElementById("src" + inc).style.transform = "translate("+tempX+"px,"+tempY+"px)";
 	document.getElementById("src" + inc).style.zIndex = positions[inc-1][1];
+
+	//On update l'audio
+
 	AudioUpdatePos(inc, positions);
 }
 
+//fonction permettant d'update l'orientation
+
 function UpdateOr(value) {
+
+	//On affiche dans le bon sens le triangle représentatif de l'utilisateur
+
 	document.getElementsByClassName("UserPos")[0].style.transform = "translate(-9px, 95px) RotateZ(-"+value+"deg)";
+
+	//On update l'audio
+
 	AudioUpdateOr(value);
 }
 
+//fonction permettant d'inclure un fichier html dans un autre
 
 function includeHTML() {
   var z, i, elmnt, file, xhttp;
@@ -105,6 +164,9 @@ function includeHTML() {
 	      xhttp.onreadystatechange = function() {
 	        if (this.readyState == 4) {
 	          if (this.status == 200) {
+
+	          	//on y passe nbSound fois pour avoir le nombre de blocs correspondants dans le code html
+
 	          	for (var j = 0; j < nbSound; j++) {
 	          		elmnt.innerHTML += this.responseText;	          	
 	          	}
@@ -122,8 +184,16 @@ function includeHTML() {
   }
 }
 
+//fonction permettant de set des ids différentes aux blocs générés par la fonction includeHTML()
+
 function SetId() {
+
+	//On récupère les différents blocs ajoutés
+
 	var bloc = document.getElementsByClassName("Flex");
+
+	//On itère sur chacun d'eux et on rajoute l'itérateur à tous les ids des éléments de chaque bloc
+
   	for (i = 1; i <= bloc.length; i++) {
 	  	var elem = bloc[i-1].getElementsByTagName("*");
 	  	for (j = 0; j < elem.length; j++) {
